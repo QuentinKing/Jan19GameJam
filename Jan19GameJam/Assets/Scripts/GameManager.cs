408063317bce9330c126af3450cc6ccd33f1624e
Alexander Cherkaski <35356424+cherkaski@users.noreply.github.com>
2019-01-19 14:59:17 -0500

    Add files via upload

---
+++ Jan19GameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.gameObject.transform.position.y < -1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            //Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
